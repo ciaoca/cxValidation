6124d2260f983ed749fd1c86b8c0ed39613f0d46
ciaoca <ciaoca@gmail.com>
2020-11-11 15:49:56 +0800

    clear

--- js/cxvalidation.js
+++ js/cxvalidation.js
@@ -545,7 +545,6 @@
 
     if (typeof $.cxDialog === 'function') {
       $.cxDialog({
-        title: '提示',
         info: message,
         ok: typeof callback === 'function' ? callback : function() {}
       });
